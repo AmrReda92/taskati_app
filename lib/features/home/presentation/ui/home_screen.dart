7dc9b028f0468aedc5fc8188aec25f202a4ab3ee
Amr Reda <amr.reda2014@yahoo.com>
2025-11-03 13:28:27 +0200

    adjust theme light and dark on app

--- lib/features/home/presentation/ui/home_screen.dart
+++ lib/features/home/presentation/ui/home_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:taskati_application/core/costant/app_string.dart';
 import 'package:taskati_application/core/theme/app_style.dart';
+import 'package:taskati_application/core/theme/cubit/theme_cubit.dart';
 import 'package:taskati_application/features/home/data/home_repo.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -24,6 +26,11 @@ class HomeScreen extends StatelessWidget {
                   CircleAvatar(
                     radius: 40,
                     backgroundImage: Image.file(File(HomeRepo.getUserData().userImage)).image,
+                  ),
+                  Switch(value: context.read<ThemeCubit>().appTheme==ThemeData.dark(), onChanged: (v)
+                  {
+                    context.read<ThemeCubit>().changeTheme();
+                  }
                   )
                 ],
               )
--- lib/taskati_app.dart
+++ lib/taskati_app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:taskati_application/core/routes/app_routs.dart';
 import 'package:taskati_application/core/routes/routes.dart';
+import 'package:taskati_application/core/theme/cubit/theme_cubit.dart';
 import 'package:taskati_application/features/login/presentation/ui/login_screen.dart';
 import 'package:taskati_application/features/splash/presentation/ui/splash_screen.dart';
 
@@ -11,14 +13,22 @@ class TaskatiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(800, 1600),
-    minTextAdapt: true,
-    splitScreenMode: true,
-      child: MaterialApp(
-        onGenerateRoute: AppRoutes.routes
-         ,
-        debugShowCheckedModeBanner: false,
-        initialRoute: Routes.splashScreen ,
+      designSize: const Size(800, 1600),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: BlocProvider(
+        create: (context) => ThemeCubit(),
+        child: BlocBuilder<ThemeCubit, ThemeState>(
+          builder: (context, state) {
+            return MaterialApp(
+              theme: context.read<ThemeCubit>().appTheme,
+              onGenerateRoute: AppRoutes.routes
+              ,
+              debugShowCheckedModeBanner: false,
+              initialRoute: Routes.homeScreen,
+            );
+          },
+        ),
       ),
     );
   }
