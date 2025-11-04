b347853c76a2b0b162663266e43592d59b668709
Amr Reda <amr.reda2014@yahoo.com>
2025-11-04 21:21:37 +0200

    generate hive adapter

--- lib/core/models/task_model.dart
+++ lib/core/models/task_model.dart
@@ -1,15 +1,25 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+part 'task_model.g.dart';
 
-class TaskModel{
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
 
+  @HiveField(0)
   final String title ;
+  @HiveField(1)
   final String description ;
+  @HiveField(2)
   final String date ;
+  @HiveField(3)
   final String startTime ;
+  @HiveField(4)
   final String endTime ;
+  @HiveField(5)
   final Color color;
+  @HiveField(6)
   final String status;
 
   TaskModel({required this.color, required this.title,required this.description,required this.date, required this.startTime,
